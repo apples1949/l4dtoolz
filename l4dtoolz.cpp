c3c7ec8c4a5d743c741265cdc4052f2f98471468
apples1949 <apples1949@163.com>
2023-09-03 11:38:10 +0800

    upload

--- l4dtoolz.cpp
+++ l4dtoolz.cpp
@@ -274,6 +274,8 @@ err_sv:
 	((void (*)(void *, int, netadr_s *, const char *, Handler *))net[0][12])(net, 99, &adr, "l4dtoolz", new Handler);
 #endif
 	return true;
+	engine->ServerCommand("sv_setmax 31\n");
+	Msg("[L4DToolZ] maxplayers default set to 31\n");
 }
 
 void l4dtoolz::Unload(){
